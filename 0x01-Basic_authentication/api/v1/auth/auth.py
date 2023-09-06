65fc77695ad394c823c9380eccfed77024f64c07
Cruwmorgan <morganokeke@gmail.com>
2023-09-06 06:31:21 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -56,6 +56,9 @@ class Auth:
         if request is None:
             return None
 
+        if (request.headers.get('Authorization')) is None:
+            return None
+
         return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
