575d80f3ed927f7ae1d0e561bbbee706e76b5e7b
ctuwmorgan <morganokeke@gmail.com>
2023-09-12 10:10:48 +0000

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -51,7 +51,7 @@ class DB:
 
         return user
 
-    def find_user_by(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs: Dict[str, str]) -> User:
         """Get user from DB
             Args:
                 kwargs: Arbitrary keyword arguments
