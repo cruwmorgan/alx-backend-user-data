924b76b18279046548161ed92d59511dbb3b24e5
ctuwmorgan <morganokeke@gmail.com>
2023-09-12 14:39:41 +0000

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,39 @@
 """
     A Flask APp
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort, redirect, make_response
+from sqlalchemy.orm.exc import NoResultFound
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
-def hello():
+def hello() -> str:
+    """ Greeting in French """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def new_register() -> str:
+    """ Create a new user """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        msg = {"message": "email already registered"}
+        return jsonify(msg), 400
+
+    msg = {"email": user.email, "message": "user created"}
+
+    return jsonify(msg)
 
 
 if __name__ == "__main__":
